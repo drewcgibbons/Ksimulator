81bf0a958f1500e21f2e2b87f93bf44502b72573
Drew Gibbons <drewcgibbons@gmail.com>
2020-05-12 05:23:48 -0500

    Regexin cleanup

--- Baseball/players/cleanbatterdata.py
+++ Baseball/players/cleanbatterdata.py
@@ -1,9 +1,12 @@
 import fileinput
 import re
 
-with fileinput.FileInput("lineups.txt", inplace=True) as file:
-    for line in file:
-        new_line = re.match(",\w+.\w+\\w+,", line)
+with open("lineups.txt", 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    for line in lines:
+
+        new_line = re.match("\w+,\w+.\w+\\w+.\w+", line, re.U)
+
         print(new_line)
 
 
