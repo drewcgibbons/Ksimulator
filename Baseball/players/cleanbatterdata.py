d87dfd48ce4a1cbd3101baa7cf2521de24ec18a1
Drew Gibbons <drewcgibbons@gmail.com>
2020-05-12 06:52:58 -0500

    Added batter data

--- Baseball/players/cleanbatterdata.py
+++ Baseball/players/cleanbatterdata.py
@@ -3,10 +3,13 @@ import re
 
 with open("lineups.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
+    # writefile = open("batters.txt", 'w+', encoding='utf-8')
+
     for line in lines:
-
-        new_line = re.match("\w+,\w+.\w+\\w+.\w+", line, re.U)
-
-        print(new_line)
-
+        teamname = re.match("[A-Z]{2,3}", line, re.U)
+        nl = re.match("(\d+,\w+,[\w\.]+.\w+)[*#]?\\\\\w+(.+)", line, re.U)
+        if nl is not None:
+            print(nl.group(1) + nl.group(2))
+        if teamname is not None:
+            print("\n" + str(teamname.group(0)))
 
